241b52a31f702d2f01e8153f7b6b82c6068c41f2
corymhall <43035978+corymhall@users.noreply.github.com>
2025-03-01 14:45:04 -0500

    update token

--- .projenrc.ts
+++ .projenrc.ts
@@ -21,6 +21,7 @@ const project = new TypeScriptComponent({
     label: 'auto-approve',
     allowedUsernames: ['corymhall', 'renovate[bot]'],
   },
+  githubReleaseToken: '${{ secrets.PROJEN_GITHUB_TOKEN }}',
   packageManager: NodePackageManager.NPM,
   deps: ['@pulumi/pulumi', '@pulumi/aws-native', '@pulumi/aws'],
   devDeps: ['camelcase', '@hallcor/pulumi-projen-project-types'],
