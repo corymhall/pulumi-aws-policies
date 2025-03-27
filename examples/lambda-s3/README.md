58305b181f83e1260d324a8f9d6cd299a2660c36
corymhall <43035978+corymhall@users.noreply.github.com>
2025-03-27 14:41:27 -0400

    chore: upgrade pulumi to 3.159.0

---
+++ examples/lambda-s3/README.md
@@ -0,0 +1,15 @@
+# AWS Lambda to S3 Example
+
+## Getting Started
+
+Install dependencies
+
+```console
+$ npm install
+```
+
+Deploy example
+
+```console
+$ pulumi up
+```
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-import * as path from 'path';
-import { componentProviderHost } from '@pulumi/pulumi/provider/experimental';
-
-componentProviderHost(path.join(__dirname, '../')).catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+export * from './policies';
